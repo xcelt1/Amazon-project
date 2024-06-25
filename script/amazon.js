33d397a1247774d02246ecc2a05120a4458d6df7
xcelt1 <ugohjason1900@gmail.com>
2024-06-25 11:12:28 +0100

    Made the cart quantity interactive

--- script/amazon.js
+++ script/amazon.js
@@ -1,32 +1,32 @@
-const products = [
-    {
-        image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        priceCents: 1090
-},
-{
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating:{
-        stars: 4,
-        count: 127,
-    },
-    priceCents: 2095,
-},
-{
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating:{
-        stars: 4.5,
-        count: 56,
-    },
-    priceCents: 799,
-},
-];
+// const products = [
+//     {
+//         image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//         name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//         rating: {
+//             stars: 4.5,
+//             count: 87
+//         },
+//         priceCents: 1090
+// },
+// {
+//     image: "images/products/intermediate-composite-basketball.jpg",
+//     name: "Intermediate Size Basketball",
+//     rating:{
+//         stars: 4,
+//         count: 127,
+//     },
+//     priceCents: 2095,
+// },
+// {
+//     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//     name: "Adults Plain Cotton T-Shirt - 2 Pack",
+//     rating:{
+//         stars: 4.5,
+//         count: 56,
+//     },
+//     priceCents: 799,
+// },
+// ];
 
 let productsHtml = '';
 
@@ -75,7 +75,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button data-product-id = "${product.id}" class="add-to-cart-button button-primary js-add-to-cart-btn">
             Add to Cart
           </button>
     </div>`
@@ -83,3 +83,36 @@ products.forEach((product) => {
 
 document.querySelector('.js-product-grid')
 .innerHTML = productsHtml;
+
+document.querySelectorAll('.js-add-to-cart-btn')
+.forEach((button) => {
+  button.addEventListener('click', () =>{
+    let productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if(productId === item.productId){
+        matchingItem = item;
+      }
+    });
+
+
+    if(matchingItem){
+      matchingItem.Quantity++;
+    }
+    else{
+      cart.push({
+        productId: productId,
+        quantity: 1,
+      })
+    };
+    
+  let cartQuantity = 0;
+  cart.forEach((cartItem) =>{
+    cartQuantity += cartItem.quantity;
+  })
+  document.querySelector('.js-cart-quantity')
+  .innerHTML = cartQuantity;
+  })
+});
